c235dfadb332c85577a6aac429f88bbf6ab21c8b
Hamrless04 <devadathks004@gmail.com>
2024-08-07 00:00:18 +0530

    register form added

--- app/patients/[userid]/register/page.tsx
+++ app/patients/[userid]/register/page.tsx
@@ -14,7 +14,7 @@ const Register = () => {
       alt="patient"
       className="mb-12 h-10 w-fit"
       />
-    {/*<PatientForm />*/}
+    
     <div className='text-14-regular mt-20 flex justify-between'>
       <p className='justify-items-end text-dark-600 xl:text-left'>
     © 2024 CareFlow
