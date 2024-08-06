78299a81cc26cb503ebe72df9a9cfd6a43ad3646
Hamrless04 <devadathks004@gmail.com>
2024-08-06 23:39:54 +0530

    login page database connection done , patient page created

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -4,13 +4,14 @@ import { users } from "../appwrite.config"
 export const createUser =async (user: CreateUserParams)=> {
     try {
         const newUser = await users.create(
-            ID.unique(),user.email , 
+            ID.unique(),
+            user.email , 
             user.phone,
             undefined,
             user.name
          )
         
-    } catch (error) {
+    } catch (error : any) {
         if(error && error?.code === 409){
             const document = await users.list([
         Query.equal('email',[user.email])
